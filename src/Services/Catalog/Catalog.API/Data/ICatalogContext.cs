b4689ee65434c5df5c1f6545ce5ffa434bc44b54
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-05-19 20:55:11 +0530

    Developing Data Layer - Connect Mongo Docker Container from Catalog.API

---
+++ src/Services/Catalog/Catalog.API/Data/ICatalogContext.cs
@@ -0,0 +1,12 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public interface ICatalogContext
+    {
+        
+        IMongoCollection<Product> Products { get;}
+    }
+}
