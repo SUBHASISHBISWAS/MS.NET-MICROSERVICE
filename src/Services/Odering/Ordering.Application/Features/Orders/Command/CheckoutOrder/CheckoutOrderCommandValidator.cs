49f461e1679475df7f963735202806b860f59fda
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-05-30 18:39:19 +0530

    Developing Ordering.Application Layer - Application Command Features - Checkout

---
+++ src/Services/Odering/Ordering.Application/Features/Orders/Command/CheckoutOrder/CheckoutOrderCommandValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Command.CheckoutOrder
+{
+    public class CheckoutOrderCommandValidator : AbstractValidator<CheckoutOrderCommand>
+    {
+        public CheckoutOrderCommandValidator()
+        {
+            RuleFor(p => p.UserName).NotEmpty().
+                WithMessage("{UserName} is Required").
+                NotNull().MaximumLength(50).
+                WithMessage("{UserName} must not exceed 50 Character");
+
+
+            RuleFor(p => p.EmailAddress).NotEmpty().
+                WithMessage("{EmailAddress} is Required");
+
+            RuleFor(p => p.TotalPrice).NotEmpty().
+                WithMessage("{TotalPrice} should not be Empty").
+                GreaterThan(0).
+                WithMessage("{TotalPrice} should  be > 0");
+        }
+    }
+}
