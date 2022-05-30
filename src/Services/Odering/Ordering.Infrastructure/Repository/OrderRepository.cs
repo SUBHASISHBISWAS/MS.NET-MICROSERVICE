2907b73ae3f1adafff94febf7f8b4a4012ad3cda
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-05-30 23:34:55 +0530

    Developing Ordering.Infrastructure Layer in Clean Architecture - Repositories

---
+++ src/Services/Odering/Ordering.Infrastructure/Repository/OrderRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistance;
+using Ordering.Domain.Entities;
+using Ordering.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repository
+{
+    public  class OrderRepository : RepositoryBase<Order>,IOrderRepository
+    {
+        public OrderRepository(OrderContext context):base(context)
+        {
+
+        }
+
+        public async Task<IEnumerable<Order>> GetOrderByUserName(string userName)
+        {
+            var orderList = await _dbContext.Orders.Where(o => o.UserName == userName).ToListAsync();
+            return orderList;
+        }
+    }
+}
