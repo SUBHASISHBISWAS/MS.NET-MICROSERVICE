846dba5223f43ae1b9ea88b91f20500c7194705d
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-05-30 22:32:47 +0530

    Developing Ordering.Infrastructure Layer in Clean Architecture - Persistence

---
+++ src/Services/Odering/Ordering.Infrastructure/Persistence/OrderContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Common;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContext:DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+
+        public OrderContext(DbContextOptions<OrderContext> options):base(options)
+        {
+
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<EntityBase>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        entry.Entity.CreatedBy = "subhasish";
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        entry.Entity.LastModifiedBy = "subhasish";
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
