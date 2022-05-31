cf2c7b2d9db177958dd7b11ebd65f1d696fd8816
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-05-31 13:16:46 +0530

    Adding EF Core Migrations for Code-First Approach in Ordering Microservices

--- src/Services/Odering/Odering.API/Startup.cs
+++ src/Services/Odering/Odering.API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Ordering.Application;
+using Ordering.Infrastructure;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,7 +28,7 @@ namespace Odering.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddApplicationServices();
-            //services.AddInfrastructureServices(Configuration);
+            services.AddInfrastructureServices(Configuration);
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
