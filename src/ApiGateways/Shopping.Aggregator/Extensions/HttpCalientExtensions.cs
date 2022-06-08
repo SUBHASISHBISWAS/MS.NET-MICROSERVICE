5e789dbf54255aa9f4afc807329bb43ee52a8e55
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-06-08 13:04:07 +0530

    Developing Service Classes for Consuming Internal Microservices in Shopping.Aggr

---
+++ src/ApiGateways/Shopping.Aggregator/Extensions/HttpCalientExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Extensions
+{
+    public static class HttpCalientExtensions
+    {
+        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
+        {
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException($"Something went wrong calling the API:{response.ReasonPhrase}");
+            }
+
+            var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            return JsonSerializer.Deserialize<T>(content,new JsonSerializerOptions { PropertyNameCaseInsensitive=true});
+        }
+    }
+}
---
+++ src/ApiGateways/Shopping.Aggregator/Services/ICatalogService.cs
@@ -0,0 +1,17 @@
+﻿using Shopping.Aggregator.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Services
+{
+    public interface ICatalogService
+    {
+        Task<IEnumerable<CatalogModel>> GetCatalog();
+
+        Task<IEnumerable<CatalogModel>> GetCatelogByCategory(string categoty);
+
+        Task<CatalogModel> GetCatelog(string id);
+
+    }
+}
