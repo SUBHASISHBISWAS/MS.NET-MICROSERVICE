0ba05f9a61873eb0461ac65ab124f10a9412f552
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2024-05-10 11:00:41 +0530

    Author Code Updated

---
+++ Udemy/ CQRS-Event-Sourcing/Sean Campbell/Code/Section 5-Aggregate/PostAggregate Part 2/SM-Post/Post.Cmd/Post.Cmd.Api/Commands/EditCommentCommand.cs
@@ -0,0 +1,9 @@
+namespace Post.Cmd.Api.Commands
+{
+    public class EditCommentCommand : BaseCommand
+    {
+        public Guid CommentId { get; set; }
+        public string Comment { get; set; }
+        public string Username { get; set; }
+    }
+}
