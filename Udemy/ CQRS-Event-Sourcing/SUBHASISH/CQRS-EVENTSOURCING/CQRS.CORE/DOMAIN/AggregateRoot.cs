40dc5a6c4c725137c16c1f24e811dfad1cb49c80
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2024-05-10 10:56:15 +0530

    The most significant changes involve the addition of two new files, `AggregateRoot.cs` and `PostAggregate.cs`, which are integral to the implementation of the CQRS and Event Sourcing patterns in the system. The `AggregateRoot.cs` file defines a base class for all aggregate root entities, while the `PostAggregate.cs` file extends this base class to represent a Post in the system. The `PostAggregate` class includes methods for various operations on a post, each of which raises an event and applies the event to change the state of the post. The class also maintains a dictionary of comments added to the post and includes checks to ensure that only active posts can be modified and that users can only modify their own posts and comments.
    Changes:
    
    1. The `POST.CMD.DOMAIN.csproj` file has been modified to include a `PropertyGroup` tag. The details of the properties are not provided in the diff.
    
    2. A new file `AggregateRoot.cs` has been added. This file defines an abstract class `AggregateRoot` which is a base class for all aggregate root entities in a CQRS based system.
    
    3. A new file `PostAggregate.cs` has been added. This file defines a class `PostAggregate` which extends the `AggregateRoot` class and represents a Post in the system.
    
    4. The `PostAggregate` class uses events defined in the `POST.COMMON.EVENTS` namespace. These events are not shown in the diff.
    
    5. The `PostAggregate` class maintains a private dictionary `_comments` to keep track of comments added to the post.
    
    6. The `PostAggregate` class includes checks to ensure that only active posts can be modified and that users can only modify their own posts and comments.

---
+++ Udemy/ CQRS-Event-Sourcing/SUBHASISH/CQRS-EVENTSOURCING/CQRS.CORE/DOMAIN/AggregateRoot.cs
@@ -0,0 +1,59 @@
+﻿using CQRS.CORE.EVENTS;
+using System.Reflection;
+
+namespace CQRS.CORE.DOMAIN
+{
+    public abstract class AggregateRoot
+    {
+        protected Guid _id;
+
+        private readonly List<BaseEvent> _changes = new();
+
+        public Guid Id => _id;
+
+        public int Version { get; set; } = -1;
+
+
+        public IEnumerable<BaseEvent> GetUncommittedChanges()
+        {
+            return _changes;
+        }
+
+        public void MarkChangesAsCommitted()
+        {
+            _changes.Clear();
+        }
+
+        private void ApplyChange(BaseEvent @event, bool isNew)
+        {
+            MethodInfo? method = this.GetType().GetMethod("Apply", new Type[] { @event.GetType() });
+
+            if (method == null)
+            {
+                throw new ArgumentNullException(nameof(method), $"The Apply method was not found in the aggregate for {@event.GetType().Name}!");
+            }
+
+            method.Invoke(this, new object[] { @event });
+
+            if (isNew)
+            {
+                _changes.Add(@event);
+            }
+        }
+
+        protected void RaiseEvent(BaseEvent @event)
+        {
+            ApplyChange(@event, true);
+        }
+
+        /// <summary>Replays the event.</summary>
+        /// <param name="events">The events.</param>
+        public void ReplayEvent(IEnumerable<BaseEvent> events)
+        {
+            foreach (BaseEvent e in events)
+            {
+                ApplyChange(e, false);
+            }
+        }
+    }
+}
