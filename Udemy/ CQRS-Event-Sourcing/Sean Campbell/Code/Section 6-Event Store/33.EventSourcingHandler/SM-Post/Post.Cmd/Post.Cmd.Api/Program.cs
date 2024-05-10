9317aa74ad513ade4347e5b870c1a4b0c8e6fd8c
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2024-05-10 20:42:50 +0530

    Author Code Added

---
+++ Udemy/ CQRS-Event-Sourcing/Sean Campbell/Code/Section 6-Event Store/33.EventSourcingHandler/SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
@@ -0,0 +1,34 @@
+
+using CQRS.Core.Domain;
+using CQRS.Core.Infrastructure;
+using Post.Cmd.Infrastructure.Config;
+using Post.Cmd.Infrastructure.Repositories;
+using Post.Cmd.Infrastructure.Stores;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.Configure<MongoDbConfig>(builder.Configuration.GetSection(nameof(MongoDbConfig)));
+builder.Services.AddScoped<IEventStoreRepository, EventStoreRepository>();
+builder.Services.AddScoped<IEventStore, EventStore>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
