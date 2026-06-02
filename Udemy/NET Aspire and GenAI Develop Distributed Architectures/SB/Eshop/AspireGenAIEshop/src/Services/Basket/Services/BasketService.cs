96d0099de993bf3bad67b43218ea5819060713b5
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2026-06-03 00:00:13 +0530

    Add BasketEndpoints for managing user shopping carts
    - Implemented HTTP endpoints (`MapBasketEndpoints`) for Get, Post (Upsert), and Delete operations.
    - Secured endpoints with authorization and added appropriate response handling.
    - Updated `Basket.csproj` to include new `Endpoints` folder.
    - Commented out product price and name updates during basket operations in `BasketService`.

--- Udemy/NET Aspire and GenAI Develop Distributed Architectures/SB/Eshop/AspireGenAIEshop/src/Services/Basket/Services/BasketService.cs
+++ Udemy/NET Aspire and GenAI Develop Distributed Architectures/SB/Eshop/AspireGenAIEshop/src/Services/Basket/Services/BasketService.cs
@@ -11,21 +11,20 @@ public class BasketService(IDistributedCache cache)
         return string.IsNullOrEmpty(basket) ? null :
             JsonSerializer.Deserialize<ShoppingCart>(basket);
     }
-    /*
+  
     public async Task UpdateBasket(ShoppingCart basket)
     {
         // Before update(Add/remove Item) into SC, we should call Catalog ms GetProductById method
         // Get latest product information and set Price and ProductName when adding item into SC
         foreach (var item in basket.Items)
         {
-            var product = await catalogApiClient.GetProductById(item.ProductId);
-            item.Price = product.Price;
-            item.ProductName = product.Name;
+            //var product = await catalogApiClient.GetProductById(item.ProductId);
+            //item.Price = product.Price;
+            //item.ProductName = product.Name;
         }
 
         await cache.SetStringAsync(basket.UserName, JsonSerializer.Serialize(basket));
     }
-    */
     public async Task DeleteBasket(string userName)
     {
         await cache.RemoveAsync(userName);
